37913bec33e39aa34e4ae02f3cbef077989825c3
amirali <amirali.kaboli@gmail.com>
2024-04-08 12:43:27 +0100

    add HtIdx & VecIdx

--- include/build.h
+++ include/build.h
@@ -7,7 +7,51 @@
 
 using namespace std;
 
-vector<pair<long, long>> load(string path, bool reversed = false, bool skip_first_line = false) {
+int partition(pair<vector<long>, vector<long>> &tuples, int low, int high) {
+	int pivot_idx = (high + low) >> 1;
+	long pivot = tuples.first[pivot_idx];
+	swap(tuples.first[pivot_idx], tuples.first[high]);
+	swap(tuples.second[pivot_idx], tuples.second[high]);
+	int i = low - 1;
+
+	for (int j = low; j <= high - 1; ++j) {
+		if (tuples.first[j] < pivot) {
+			++i;
+			swap(tuples.first[i], tuples.first[j]);
+			swap(tuples.second[i], tuples.second[j]);
+		}
+	}
+	swap(tuples.first[i + 1], tuples.first[high]);
+	swap(tuples.second[i + 1], tuples.second[high]);
+	return i + 1;
+}
+
+void insertion_sort(std::pair<std::vector<long>, std::vector<long>>& tuples, int low, int high) {
+	for (int i = low + 1; i <= high; i++) {
+		long key1 = tuples.first[i];
+		long key2 = tuples.second[i];
+		int j = i - 1;
+		while (j >= low && tuples.first[j] > key1) {
+			tuples.first[j + 1] = tuples.first[j];
+			tuples.second[j + 1] = tuples.second[j];
+			j--;
+		}
+		tuples.first[j + 1] = key1;
+		tuples.second[j + 1] = key2;
+	}
+}
+
+void sort(pair<vector<long>, vector<long>> &tuples, int low, int high) {
+	if (high - low < 50) {
+		insertion_sort(tuples, low, high);
+	} else {
+		int pi = partition(tuples, low, high);
+		sort(tuples, low, pi - 1);
+		sort(tuples, pi + 1, high);
+	}
+}
+
+void load(string path, vector<pair<long, long>> &tuples, bool reversed = false, bool skip_first_line = false) {
 	ifstream in(path);
 	if (!in)
 		cerr << "Cannot open the File : " << path << endl;
@@ -15,7 +59,6 @@ vector<pair<long, long>> load(string path, bool reversed = false, bool skip_firs
 	string token;
 	if (skip_first_line)
 		getline(in, line);
-	vector<pair<long, long>> tuples;
 	while (getline(in, line)) {
 		stringstream ss(line);
 		getline(ss, token, '|');
@@ -28,7 +71,31 @@ vector<pair<long, long>> load(string path, bool reversed = false, bool skip_firs
 			tuples.push_back({lll, rrr});
 	}
 	in.close();
-	return tuples;
+}
+
+void load(string path, pair<vector<long>, vector<long>> &tuples, bool reversed = false, bool skip_first_line = false) {
+	ifstream in(path);
+	if (!in)
+		cerr << "Cannot open the File : " << path << endl;
+	string line;
+	string token;
+	if (skip_first_line)
+		getline(in, line);
+	while (getline(in, line)) {
+		stringstream ss(line);
+		getline(ss, token, '|');
+		auto lll = stol(token);
+		getline(ss, token, '|');
+		auto rrr = stol(token);
+		if (reversed) {
+			tuples.first.push_back(rrr);
+			tuples.second.push_back(lll);
+		} else {
+			tuples.first.push_back(lll);
+			tuples.second.push_back(rrr);
+		}
+	}
+	in.close();
 }
 
 bool cmp(const pair<long, long> &a, const pair<long, long> &b) {
@@ -90,4 +157,32 @@ void build_trie(vector<pair<long, long>> &tuples, vector<pair<long, long>> &trie
 void build_trie(vector<pair<long, long>> &tuples, phmap::flat_hash_map<long, long> &trie) {
 	for (auto &ent: tuples)
 		trie[ent.first] = ent.second;
+}
+
+void build_trie(pair<vector<long>, vector<long>> &tuples, phmap::flat_hash_map<long, pair<int, int>> &trie) {
+	sort(tuples, 0, tuples.first.size() - 1);
+	auto &last_key = tuples.first[0];
+	auto last_i = 0;
+	for (int i = 1; i < tuples.first.size(); ++i) {
+		if (tuples.first[i] != last_key) {
+			trie[last_key] = {last_i, i};
+			last_key = tuples.first[i];
+			last_i = i;
+		}
+	}
+	trie[last_key] = {last_i, tuples.first.size()};
+}
+
+void build_trie(pair<vector<long>, vector<long>> &tuples, vector<tuple<long, int, int>> &trie) {
+	sort(tuples, 0, tuples.first.size() - 1);
+	auto &last_key = tuples.first[0];
+	auto last_i = 0;
+	for (int i = 1; i < tuples.first.size(); ++i) {
+		if (tuples.first[i] != last_key) {
+			trie.push_back({last_key, last_i, i});
+			last_key = tuples.first[i];
+			last_i = i;
+		}
+	}
+	trie.push_back({last_key, last_i, tuples.first.size()});
 }
--- queries/ours_job_star.cpp
+++ queries/ours_job_star.cpp
@@ -13,14 +13,19 @@ int iters;
 bool verbose;
 
 void htht_benchmark() {
-	auto R_tuples = load("../data/JOB/movie_info_idx_compact.csv");
-	auto S_tuples = load("../data/JOB/title_compact.csv");
-	auto T_tuples = load("../data/JOB/movie_companies_compact.csv");
+	vector<pair<long, long>> movie_info_idx, title, movie_companies;
+	load("../data/JOB/movie_info_idx_compact.csv", movie_info_idx);
+	load("../data/JOB/title_compact.csv", title);
+	load("../data/JOB/movie_companies_compact.csv", movie_companies);
 
 	auto trie_timer = HighPrecisionTimer();
 	auto query_timer = HighPrecisionTimer();
 
-	for (size_t i = 0; i < iters; ++i) {
+	for (size_t x = 0; x < iters; ++x) {
+		auto R_tuples = movie_info_idx;
+		auto S_tuples = title;
+		auto T_tuples = movie_companies;
+
 		trie_timer.Reset();
 		auto R_trie = phmap::flat_hash_map<long, phmap::flat_hash_map<long, bool>>();  // {x -> {a -> 1}}
 		build_trie(R_tuples, R_trie);
@@ -61,14 +66,19 @@ void htht_benchmark() {
 }
 
 void htvec_benchmark() {
-	auto R_tuples = load("../data/JOB/movie_info_idx_compact.csv");
-	auto S_tuples = load("../data/JOB/title_compact.csv");
-	auto T_tuples = load("../data/JOB/movie_companies_compact.csv");
+	vector<pair<long, long>> movie_info_idx, title, movie_companies;
+	load("../data/JOB/movie_info_idx_compact.csv", movie_info_idx);
+	load("../data/JOB/title_compact.csv", title);
+	load("../data/JOB/movie_companies_compact.csv", movie_companies);
 
 	auto trie_timer = HighPrecisionTimer();
 	auto query_timer = HighPrecisionTimer();
 
-	for (size_t i = 0; i < iters; ++i) {
+	for (size_t x = 0; x < iters; ++x) {
+		auto R_tuples = movie_info_idx;
+		auto S_tuples = title;
+		auto T_tuples = movie_companies;
+
 		trie_timer.Reset();
 		auto R_trie = phmap::flat_hash_map<long, vector<long>>();  // {x -> {a -> 1}}
 		build_trie(R_tuples, R_trie);
@@ -103,14 +113,19 @@ void htvec_benchmark() {
 }
 
 void vecvec_benchmark() {
-	auto R_tuples = load("../data/JOB/movie_info_idx_compact.csv");
-	auto S_tuples = load("../data/JOB/title_compact.csv");
-	auto T_tuples = load("../data/JOB/movie_companies_compact.csv");
+	vector<pair<long, long>> movie_info_idx, title, movie_companies;
+	load("../data/JOB/movie_info_idx_compact.csv", movie_info_idx);
+	load("../data/JOB/title_compact.csv", title);
+	load("../data/JOB/movie_companies_compact.csv", movie_companies);
 
 	auto trie_timer = HighPrecisionTimer();
 	auto query_timer = HighPrecisionTimer();
 
-	for (size_t i = 0; i < iters; ++i) {
+	for (size_t x = 0; x < iters; ++x) {
+		auto R_tuples = movie_info_idx;
+		auto S_tuples = title;
+		auto T_tuples = movie_companies;
+
 		trie_timer.Reset();
 		vector<pair<long, vector<long>>> R_trie;  // {x -> {a -> 1}}
 		build_trie(R_tuples, R_trie);
@@ -144,15 +159,127 @@ void vecvec_benchmark() {
 	cout << "VecVec: " << round(trie_timer.GetMean(0)) << " / " << round(query_timer.GetMean(0)) << " ms" << endl;
 }
 
-void vec_benchmark() {
-	auto R_tuples = load("../data/JOB/movie_info_idx_compact.csv");
-	auto S_tuples = load("../data/JOB/title_compact.csv");
-	auto T_tuples = load("../data/JOB/movie_companies_compact.csv");
+void htidx_benchmark() {
+	pair<vector<long>, vector<long>> movie_info_idx, title, movie_companies;
+	load("../data/JOB/movie_info_idx_compact.csv", movie_info_idx);
+	load("../data/JOB/title_compact.csv", title);
+	load("../data/JOB/movie_companies_compact.csv", movie_companies);
 
 	auto trie_timer = HighPrecisionTimer();
 	auto query_timer = HighPrecisionTimer();
 
-	for (size_t i = 0; i < iters; ++i) {
+	for (size_t x = 0; x < iters; ++x) {
+		auto R_tuples = movie_info_idx;
+		auto S_tuples = title;
+		auto T_tuples = movie_companies;
+
+		trie_timer.Reset();
+		auto R_trie = phmap::flat_hash_map<long, pair<int, int>>();  // {x -> {a -> 1}}
+		build_trie(R_tuples, R_trie);
+
+		auto S_trie = phmap::flat_hash_map<long, pair<int, int>>();  // {x -> {b -> 1}}
+		build_trie(S_tuples, S_trie);
+
+		auto T_trie = phmap::flat_hash_map<long, pair<int, int>>();  // {x -> {c -> 1}}
+		build_trie(T_tuples, T_trie);
+		trie_timer.StoreElapsedTime(0);
+
+		query_timer.Reset();
+		vector<tuple<long, long, long, long>> res;
+		for (auto &R_ent: R_trie) {
+			auto &x = R_ent.first;
+			auto &R_prime = R_ent.second;
+			if (S_trie.contains(x) && T_trie.contains(x)) {
+				auto &S_prime = S_trie.at(x);
+				auto &T_prime = T_trie.at(x);
+				for (int i = R_prime.first; i < R_prime.second; ++i) {
+					auto &a = R_tuples.second[i];
+					for (int j = S_prime.first; j < S_prime.second; ++j) {
+						auto &b = S_tuples.second[j];
+						for (int k = T_prime.first; k < T_prime.second; ++k) {
+							auto &c = T_tuples.second[i];
+							res.push_back(make_tuple(x, a, b, c));
+						}
+					}
+				}
+			}
+		}
+		query_timer.StoreElapsedTime(0);
+
+		if (verbose)
+			cerr << res.size() << endl;
+	}
+	cout << "HtIdx: " << round(trie_timer.GetMean(0)) << " / " << round(query_timer.GetMean(0)) << " ms" << endl;
+}
+
+void vecidx_benchmark() {
+	pair<vector<long>, vector<long>> movie_info_idx, title, movie_companies;
+	load("../data/JOB/movie_info_idx_compact.csv", movie_info_idx);
+	load("../data/JOB/title_compact.csv", title);
+	load("../data/JOB/movie_companies_compact.csv", movie_companies);
+
+	auto trie_timer = HighPrecisionTimer();
+	auto query_timer = HighPrecisionTimer();
+
+	for (size_t x = 0; x < iters; ++x) {
+		auto R_tuples = movie_info_idx;
+		auto S_tuples = title;
+		auto T_tuples = movie_companies;
+
+		trie_timer.Reset();
+		vector<tuple<long, int, int>> R_trie;  // {x -> {a -> 1}}
+		build_trie(R_tuples, R_trie);
+
+		auto S_trie = phmap::flat_hash_map<long, pair<int, int>>();  // {x -> {b -> 1}}
+		build_trie(S_tuples, S_trie);
+
+		auto T_trie = phmap::flat_hash_map<long, pair<int, int>>();  // {x -> {c -> 1}}
+		build_trie(T_tuples, T_trie);
+		trie_timer.StoreElapsedTime(0);
+
+		query_timer.Reset();
+		vector<tuple<long, long, long, long>> res;
+		for (auto &R_ent: R_trie) {
+			auto &x = get<0>(R_ent);
+			auto &R_l = get<1>(R_ent);
+			auto &R_r = get<2>(R_ent);
+			if (S_trie.contains(x) && T_trie.contains(x)) {
+				auto &S_prime = S_trie.at(x);
+				auto &T_prime = T_trie.at(x);
+				for (int i = R_l; i < R_r; ++i) {
+					auto &a = R_tuples.second[i];
+					for (int j = S_prime.first; j < S_prime.second; ++j) {
+						auto &b = S_tuples.second[j];
+						for (int k = T_prime.first; k < T_prime.second; ++k) {
+							auto &c = T_tuples.second[k];
+							res.push_back(make_tuple(x, a, b, c));
+						}
+					}
+				}
+			}
+		}
+		query_timer.StoreElapsedTime(0);
+
+		if (verbose)
+			cerr << res.size() << endl;
+	}
+	cout << "VecIdx: " << round(trie_timer.GetMean(0)) << " / " << round(query_timer.GetMean(0)) << " ms" << endl;
+}
+
+void vec_benchmark() {
+	vector<pair<long, long>> movie_info_idx, title, movie_companies;
+	load("../data/JOB/movie_info_idx_compact.csv", movie_info_idx);
+	load("../data/JOB/title_compact.csv", title);
+	load("../data/JOB/movie_companies_compact.csv", movie_companies);
+
+	auto trie_timer = HighPrecisionTimer();
+	auto query_timer = HighPrecisionTimer();
+
+	for (size_t x = 0; x < iters; ++x) {
+		auto R_tuples = movie_info_idx;
+		auto S_tuples = title;
+		auto T_tuples = movie_companies;
+
 		trie_timer.Reset();
 		vector<pair<long, long>> R_trie;  // {x -> {a -> 1}}
 		build_trie(R_tuples, R_trie);
@@ -185,15 +312,20 @@ void vec_benchmark() {
 	cout << "Vec: " << round(trie_timer.GetMean(0)) << " / " << round(query_timer.GetMean(0)) << " ms" << endl;
 }
 
-void pk_benchmark() {
-	auto R_tuples = load("../data/JOB/movie_info_idx_compact.csv");
-	auto S_tuples = load("../data/JOB/title_compact.csv");
-	auto T_tuples = load("../data/JOB/movie_companies_compact.csv");
+void vecpk_benchmark() {
+	vector<pair<long, long>> movie_info_idx, title, movie_companies;
+	load("../data/JOB/movie_info_idx_compact.csv", movie_info_idx);
+	load("../data/JOB/title_compact.csv", title);
+	load("../data/JOB/movie_companies_compact.csv", movie_companies);
 
 	auto trie_timer = HighPrecisionTimer();
 	auto query_timer = HighPrecisionTimer();
 
-	for (size_t i = 0; i < iters; ++i) {
+	for (size_t x = 0; x < iters; ++x) {
+		auto R_tuples = movie_info_idx;
+		auto S_tuples = title;
+		auto T_tuples = movie_companies;
+
 		trie_timer.Reset();
 		vector<pair<long, long>> R_trie;  // {x -> {a -> 1}}
 		build_trie(R_tuples, R_trie);
@@ -222,7 +354,7 @@ void pk_benchmark() {
 		if (verbose)
 			cerr << res.size() << endl;
 	}
-	cout << "PK: " << round(trie_timer.GetMean(0)) << " / " << round(query_timer.GetMean(0)) << " ms" << endl;
+	cout << "VecPK: " << round(trie_timer.GetMean(0)) << " / " << round(query_timer.GetMean(0)) << " ms" << endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -232,6 +364,8 @@ int main(int argc, char *argv[]) {
 	htht_benchmark();
 	htvec_benchmark();
 	vecvec_benchmark();
+	htidx_benchmark();
+	vecidx_benchmark();
 	vec_benchmark();
-	pk_benchmark();
+	vecpk_benchmark();
 }
