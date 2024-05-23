40f0aeca77c4c6069e131d8ce66417822708e405
amirali <amirali.kaboli@gmail.com>
2024-05-23 14:03:10 +0100

    keep all stats of a query in one file

--- stats/main.py
+++ stats/main.py
@@ -1,5 +1,6 @@
 import json
 import os
+from collections import defaultdict
 
 import pandas as pd
 
@@ -17,9 +18,10 @@ if __name__ == '__main__':
 		if filename.endswith(".log"):
 			query = filename[:-4]
 			print(query)
-			os.makedirs(query, exist_ok=True)
 			with open(os.path.join(plans_path, filename), "r") as log_file:
 				build_plan = eval(log_file.readlines()[0].strip())
+
+			stats = defaultdict(dict)
 			for rel, _, _ in build_plan:
 				if os.path.exists(os.path.join(preprocessed_data_path, query, f"{rel}.csv")):
 					df = pd.read_parquet(os.path.join(preprocessed_data_path, query, f"{rel}.parquet"))
@@ -28,7 +30,8 @@ if __name__ == '__main__':
 						os.path.join(raw_data_path, f"{abbr2rel[rel if not rel[-1].isdigit() else rel[:-1]]}.parquet")
 					)
 
-				stats = {col: int(num) for col, num in dict(df.nunique()).items()}
-				stats["rows"] = df.shape[0]
-				with open(os.path.join(here_path, query, f"{rel}.json"), "w") as stats_file:
-					json.dump(stats, stats_file, indent=2)
+				stats[rel].update({col: int(num) for col, num in dict(df.nunique()).items()})
+				stats[rel].update({"rows": len(df)})
+
+			with open(os.path.join(here_path, f"{query}.json"), "w") as stats_file:
+				json.dump(stats, stats_file, indent=2)
