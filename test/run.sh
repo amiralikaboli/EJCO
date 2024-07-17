bb825344c97f16a95fc9eea8547f9e33593adf31
amirali <amirali.kaboli@gmail.com>
2024-07-17 11:36:02 +0100

    update Q8a test files

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,3 @@
 g++ 8a_hint_vec.cpp -O3 -std=c++17 && ./a.out
-g++ 8a_hint_int.cpp -O3 -std=c++17 && ./a.out
-g++ 8a_hint_int_sort.cpp -O3 -std=c++17 && ./a.out
-g++ 8a_hint_svec.cpp -O3 -std=c++17 && ./a.out
 g++ 8a_hint_svec_vecptr.cpp -O3 -std=c++17 && ./a.out
 g++ 8a_hint_mulmap.cpp -O3 -std=c++17 && ./a.out
-g++ 8a_hint_map.cpp -O3 -std=c++17 && ./a.out
