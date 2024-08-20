b8cab3e7bcc6e0a1b6fda57e0ad0cb1892962909
amascolo <12738821+amascolo@users.noreply.github.com>
2024-08-20 21:27:04 +0100

    Remove the unused unique tags

--- sdql_gen.py
+++ sdql_gen.py
@@ -213,7 +213,9 @@ class FJSDQLGenerator(AbstractSDQLGenerator):
 			for i, col in enumerate(trie_levels[::-1]):
 				# i check isn't needed as FJ queries have 1 level of nesting - keeping it for robustness/correctness
 				hint = "" if i > 0 else f"@phmap({rel}.size) "
-				trie_value = f"{hint}{{ unique({rel}.{col}(i)) -> {trie_value} }}"
+				inner = f"{rel}.{col}(i)"
+				key = inner if "@smallvecdict" in trie_value else f"unique({inner})"
+				trie_value = f"{hint}{{ {key} -> {trie_value} }}"
 			yield f"let {self.var_mng.trie_var(rel)} = sum(<i, _> <- range({rel}.size)) {trie_value} in\n"
 
 		if not self.var_mng.is_root_rel(interm):
